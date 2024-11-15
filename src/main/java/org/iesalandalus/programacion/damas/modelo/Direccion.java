f386a561db1fd0139af126b8caf7171f8dddc81e
Rocho <rochogarseg7@gmail.com>
2024-11-15 19:45:59 +0100

    Enumerado Direccion 1

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public enum Direccion {
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+}
