32f9c29df27f9054ecc2546f35f43eb0d9624a51
Rocho <rochogarseg7@gmail.com>
2024-11-18 21:56:41 +0100

    Clase Posicion 6

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -60,4 +60,9 @@ public class Posicion {
     public int hashCode() {
         return Objects.hash(fila, columna);
     }
+
+    @Override
+    public String toString() {
+        return "fila=" + fila + ", columna=" + columna;
+    }
 }
