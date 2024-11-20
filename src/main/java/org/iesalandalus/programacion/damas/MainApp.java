320657be70886dacacb288c6e621e01e3182d070
Rocho <rochogarseg7@gmail.com>
2024-11-20 21:18:16 +0100

    Clase MainApp 2

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -10,4 +10,25 @@ public class MainApp {
     // atributo de clase de tipo Dama
     private static Dama dama;
 
+    // metodo opcines
+    public static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
+    }
+
 }
