38b91979bfca351b3533da7db5af784b733ca775
Rocho <rochogarseg7@gmail.com>
2024-11-20 18:48:44 +0100

    Clase Consola 3

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -5,4 +5,12 @@ public class Consola {
     private Consola() { // constructor privado para impedir instanciación
 
     }
+
+    public static void mostrarMenu () {
+        System.out.println("Menú principal Damas");
+        System.out.println("1. Crear dama por defecto");
+        System.out.println("2. Crear dama eligiendo el color");
+        System.out.println("3. Mover dama");
+        System.out.println("4. Salir del menú");
+    }
 }
